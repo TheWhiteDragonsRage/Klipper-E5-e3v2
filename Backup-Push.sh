95ec97d2597dd4ea7e83187b522c2acc0c2eeee3
TheWhiteDragonsRage <Dragonsrage99@gmail.com>
2022-06-15 13:25:59 +0100

    backup

---
+++ Backup-Push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ~/klipper_config/
+git add .
+git commit -m "backup"
+git push -u origin master
+
+
+#https://stackoverflow.com/questions/35942754/how-can-i-save-username-and-password-in-git
